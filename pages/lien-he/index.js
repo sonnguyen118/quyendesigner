397d9567e19febc18719e6623d8c52da0bec38ab
Sơn Nguyễn Văn <sonnguyenprofessor@gmail.com>
2022-04-08 21:42:02 +0700

    test

---
+++ pages/lien-he/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <div className="contact">test thử build website</div>
+    </Layout>
+  );
+};
+
+export default Contact;
